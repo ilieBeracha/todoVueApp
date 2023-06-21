250ee94263f050841205065e40702f02d617334e
Ilie Peter Beracha <ilieberacha@Ilies-MacBook-Pro-2.local>
2023-06-21 15:09:39 +0300

    dep

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
+
 import LandingPage from "../pages/LandingPage.vue";
 import Home from "../pages/Home.vue";
 import AddTodo from '../pages/AddTodo.vue'
